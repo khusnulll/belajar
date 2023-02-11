9a4c3c044d39da6ad5c726ec9a0ce469de57acce
Khusnul Khotimah <108590317+khusnulll@users.noreply.github.com>
2023-02-11 23:43:46 +0700

    Add files via upload

---
+++ bootstrap-part4/js/script.js
@@ -0,0 +1,55 @@
+// Event
+$('.page-scroll').on('click', function(e){
+
+    //Ambil isi href
+    var tujuan = $(this).attr('href');
+    //Tangkap elemen yang bersangkutan
+    var elemenTujuan = $(tujuan);
+
+
+    // Pindahkan scroll
+    $('html, body').animate({
+        scrollTop: elemenTujuan.offset().top - 50
+    }, 1250, 'easeInOutExpo');
+
+    e.preventDefault();
+
+});
+
+
+//Parallax
+//About
+$(window).on('load', function() {
+    $('.pKiri').addClass('pMuncul');
+    $('.pKanan').addClass('pMuncul');
+});
+
+$(window).scroll(function() {
+    var wScroll = $(this).scrollTop();
+
+    //Jumbotron
+
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+    });
+
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+    });
+
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+    });
+
+
+    //Portfolio
+    if(wScroll > $('.portfolio').offset().top - 250) {
+
+        $('.portfolio .thumbnail').each(function(i) {
+            setTimeout(function() {
+                $('.portfolio .thumbnail').eq(i).addClass('muncul');
+            }, 300 * (i+1));
+        });
+        //
+    }
+});
